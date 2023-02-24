5d8243d00ed9fa3f69e19e6cade6db43beb68531
Jan Dvorak <hello@jandvorak.me>
2023-02-24 17:02:07 +0100

    New translations user.js (Czech)

--- cs-CZ/user.js
+++ cs-CZ/user.js
@@ -1,27 +1,27 @@
 export default {
-    'user.friend.request.add': 'Přidat mezi přátele',
-    'user.friend.requests': `Máte {num, number} {num, plural,
+  'user.friend.request.add': 'Přidat mezi přátele',
+  'user.friend.requests': `Máte {num, number} {num, plural,
     zero {nových žádostí}
     one {novou žádost}
     few {nové žádosti}
     other {nových žádostí}
   } o přátelství`,
-    'user.friend.unfriend': 'Odebrat z přátel',
-    'user.block': 'Blokovat',
-    'user.unblock': 'Odblokovat',
-    'user.joined': `{gender, select,
+  'user.friend.unfriend': 'Odebrat z přátel',
+  'user.block': 'Blokovat',
+  'user.unblock': 'Odblokovat',
+  'user.joined': `{gender, select,
     male {Přidal}
     female {Přidala}
     other {Přidali}} se {date, date, long}`,
-    'user.friend.request.accept': 'Přijmout žádost o přátelství',
-    'user.friend.request.cancel': 'Zrušit žádost o přátelství',
-    'user.friend.request.deny': 'Odmítnout přátelství',
-    'user.listing': 'Seznam uživatelů',
-    'user.profile.visit': 'Navštívit profil {user}.',
-    'user.profile.avatar': 'Avatar uživatele {username}',
-    'user.friend.list.requests': 'Žádosti o přátelství',
-    'user.friend.norequests': 'Nemáte žádné žádosti o přátelství.',
-    'user.dashboardWelcome': `{dayPart, select,
+  'user.friend.request.accept': 'Přijmout žádost o přátelství',
+  'user.friend.request.cancel': 'Zrušit žádost o přátelství',
+  'user.friend.request.deny': 'Odmítnout přátelství',
+  'user.listing': 'Seznam uživatelů',
+  'user.profile.visit': 'Navštívit profil {user}.',
+  'user.profile.avatar': 'Avatar uživatele {username}',
+  'user.friend.list.requests': 'Žádosti o přátelství',
+  'user.friend.norequests': 'Nemáte žádné žádosti o přátelství.',
+  'user.dashboardWelcome': `{dayPart, select,
     morning {Dobré ráno}
     afternoon {Dobré odpoledne}
     evening {Dobrý večer}
