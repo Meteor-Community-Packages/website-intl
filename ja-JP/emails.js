c66b1b2e4d334be560711789a9ac2db0283a31b5
Jan Dvorak <hello@jandvorak.me>
2023-02-24 16:58:50 +0100

    New translations emails.js (Japanese)

--- ja-JP/emails.js
+++ ja-JP/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Socialize-starter for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
+  'email.enrollAccount.subject': 'Invitation to Socialize-starter for {username}',
+  'email.enrollAccount.message': `Hello {username}\n\n
     We would like to invite you to Socialize-starter! We have created a special personal account for you where you can access all features of Socialize-starter!
     To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'ソーシャルスターターリセットパスワード',
-    'email.resetPassword.message': `こんにちは{username}!\n\n
+  'email.resetPassword.subject': 'ソーシャルスターターリセットパスワード',
+  'email.resetPassword.message': `こんにちは{username}!\n\n
  アカウントのパスワードをリセットするリクエストを受け取りました。
         以下のリンクに従って、パスワードをリセットしてください:\n\n{url}`,
-    'email.verifyEmail.subject': 'Socialize-starterのメール認証',
-    'email.verifyEmail.message': 'こんにちは{username}!\n\n 以下のリンクをクリックして、メールアドレスを確認してください:\n\n{url}',
-    // New html email keys
-    'email.salutation': 'こんにちは{username},',
-    'email.welcome': 'Socialize-starterへようこそ！',
-    'email.signature': '敬具、<br />社交スターターチーム',
-    // Enrollment
-    'email.enrollAccount.welcome': 'ようこそ！',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Socialize-starter! We went ahead and prepared a personal account for you where you can access all features of Socialize-starter!',
-    'email.enrollAccount.button': 'アカウントにアクセスする',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Socialize-starter team',
-    // Reset password
-    'email.resetPassword.messageHtml': 'アカウントのパスワードをリセットするリクエストを受け取りました。プロセスを続行するには、下のボタンをクリックしてください。 このリクエストをまだ行っていない場合は、この電子メールを無視できますが、セキュリティ設定を確認することをお勧めします。',
-    'email.resetPassword.button': 'パスワードを再設定する',
-    // E-mail verification
-    'email.verifyEmail.messageHtml': '皆様のご参加をお待ちしております。 下のボタンをクリックして、このメールアドレスを確認してください。',
-    'email.verifyEmail.button': 'メールによる確認',
-    'email.verification': 'メールアドレスの認証',
-    'email.verifying': 'メールアドレスを確認しています。しばらくお待ちください...',
-    'email.verified': '素晴らしいお知らせです！あなたのメールアドレスが認証されました！'
-}    /* eslint-enable max-len */;
+  'email.verifyEmail.subject': 'Socialize-starterのメール認証',
+  'email.verifyEmail.message': 'こんにちは{username}!\n\n 以下のリンクをクリックして、メールアドレスを確認してください:\n\n{url}',
+  // New html email keys
+  'email.salutation': 'こんにちは{username},',
+  'email.welcome': 'Socialize-starterへようこそ！',
+  'email.signature': '敬具、<br />社交スターターチーム',
+  // Enrollment
+  'email.enrollAccount.welcome': 'ようこそ！',
+  'email.enrollAccount.messageHtml': 'We would like to invite you to Socialize-starter! We went ahead and prepared a personal account for you where you can access all features of Socialize-starter!',
+  'email.enrollAccount.button': 'アカウントにアクセスする',
+  'email.enrollAccount.signature': 'We are looking forward to see you,<br />Socialize-starter team',
+  // Reset password
+  'email.resetPassword.messageHtml': 'アカウントのパスワードをリセットするリクエストを受け取りました。プロセスを続行するには、下のボタンをクリックしてください。 このリクエストをまだ行っていない場合は、この電子メールを無視できますが、セキュリティ設定を確認することをお勧めします。',
+  'email.resetPassword.button': 'パスワードを再設定する',
+  // E-mail verification
+  'email.verifyEmail.messageHtml': '皆様のご参加をお待ちしております。 下のボタンをクリックして、このメールアドレスを確認してください。',
+  'email.verifyEmail.button': 'メールによる確認',
+  'email.verification': 'メールアドレスの認証',
+  'email.verifying': 'メールアドレスを確認しています。しばらくお待ちください...',
+  'email.verified': '素晴らしいお知らせです！あなたのメールアドレスが認証されました！'
+}  /* eslint-enable max-len */;
