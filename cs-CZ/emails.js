42298b51a8aada3094fd30321aa8139284710cc7
Jan Dvorak <hello@jandvorak.me>
2023-02-24 16:58:42 +0100

    New translations emails.js (Czech)

--- cs-CZ/emails.js
+++ cs-CZ/emails.js
@@ -3,31 +3,31 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Pozvánka do Socialize-starter pro {username}',
-    'email.enrollAccount.message': `Dobrý den {username},\n\n
+  'email.enrollAccount.subject': 'Pozvánka do Socialize-starter pro {username}',
+  'email.enrollAccount.message': `Dobrý den {username},\n\n
     Chtěli bychom Vás pozvat na Socialize-starter! Vytvořili jsme pro Vás speciální osobní účet na kterém můžete otestovat všechny funkce Socialize-starter!
 Pro aktivaci Vašeho účtu klikněte na odkaz níže:\n\n{url}`,
-    'email.resetPassword.subject': 'Socialize-starter obnovení hesla',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
+  'email.resetPassword.subject': 'Socialize-starter obnovení hesla',
+  'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
         Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Socialize-starter ověření e-mailu',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
-    // New html email keys
-    'email.salutation': 'Dobrý den {username},',
-    'email.welcome': 'Vítejte v Socialize-startéru!',
-    'email.signature': 'S pozdravem,<br />Socialize-starter tým',
-    // Enrollment
-    'email.enrollAccount.welcome': 'Vítejte na palubě',
-    'email.enrollAccount.messageHtml': 'Chtěli bychom vás pozvat do socializace startu! Šli jsme vpřed a připravili jsme pro Vás osobní účet, kde můžete přistupovat ke všem funkcím socialize-starteru!',
-    'email.enrollAccount.button': 'Zpřístupnit účet',
-    'email.enrollAccount.signature': 'Těšíme se na Vás,<br />Socialize-starter tým',
-    // Reset password
-    'email.resetPassword.messageHtml': 'Obdrželi jsme Váš požadavek na resetování Vašeho hesla, prosím klikněte na tlačítko níže k dokončení tohoto procesu.',
-    'email.resetPassword.button': 'Resetovat heslo',
-    // E-mail verification
-    'email.verifyEmail.messageHtml': 'Vítejte na palubě! Pro potvrzení této e-mailové adresy prosím klikněte na tlačítko níže.',
-    'email.verifyEmail.button': 'Potvrzení e-mailové adresy',
-    'email.verification': 'Ověření emailu',
-    'email.verifying': 'Ověřování Vašeho e-mailu. Tohle může chvilku trvat...',
-    'email.verified': 'Skvělé zprávy! Váš e-mail byl ověřen!'
-}    /* eslint-enable max-len */;
+  'email.verifyEmail.subject': 'Socialize-starter ověření e-mailu',
+  'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+  // New html email keys
+  'email.salutation': 'Dobrý den {username},',
+  'email.welcome': 'Vítejte v Socialize-startéru!',
+  'email.signature': 'S pozdravem,<br />Socialize-starter tým',
+  // Enrollment
+  'email.enrollAccount.welcome': 'Vítejte na palubě',
+  'email.enrollAccount.messageHtml': 'Chtěli bychom vás pozvat do socializace startu! Šli jsme vpřed a připravili jsme pro Vás osobní účet, kde můžete přistupovat ke všem funkcím socialize-starteru!',
+  'email.enrollAccount.button': 'Zpřístupnit účet',
+  'email.enrollAccount.signature': 'Těšíme se na Vás,<br />Socialize-starter tým',
+  // Reset password
+  'email.resetPassword.messageHtml': 'Obdrželi jsme Váš požadavek na resetování Vašeho hesla, prosím klikněte na tlačítko níže k dokončení tohoto procesu.',
+  'email.resetPassword.button': 'Resetovat heslo',
+  // E-mail verification
+  'email.verifyEmail.messageHtml': 'Vítejte na palubě! Pro potvrzení této e-mailové adresy prosím klikněte na tlačítko níže.',
+  'email.verifyEmail.button': 'Potvrzení e-mailové adresy',
+  'email.verification': 'Ověření emailu',
+  'email.verifying': 'Ověřování Vašeho e-mailu. Tohle může chvilku trvat...',
+  'email.verified': 'Skvělé zprávy! Váš e-mail byl ověřen!'
+}  /* eslint-enable max-len */;
